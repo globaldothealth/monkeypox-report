02c2a206d3af9dc87e244ddfa5668293e20d8962
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2022-06-12 21:57:29 +0100

    Remove build.sh, functionality subsumed by build.py

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eou pipefail
-python src/build.py
-Rscript src/figures/travel-history.r
-Rscript src/figures/delay-to-confirmation.r
-Rscript src/figures/genomics.r
-Rscript src/figures/age-gender.r
