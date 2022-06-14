63e3e7983d95d6fe1f157a3d51dc92d5e07f836e
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2022-06-14 22:40:13 +0100

    Add automatic nextstrain metadata upload

---
+++ src/upload_nextstrain_metadata.py
@@ -0,0 +1,55 @@
+import os
+import logging
+from typing import Optional
+from pathlib import Path
+from datetime import datetime
+
+import boto3
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+NEXTSTRAIN_MPXV = "https://nextstrain.org/monkeypox/hmpxv1"
+
+
+def fetch_metadata(link: str) -> Optional[Path]:
+    driver = webdriver.Firefox()
+    driver.get(link)
+
+    def find_button(text):
+        if not (
+            elems := [
+                e for e in driver.find_elements(By.TAG_NAME, "button") if e.text == text
+            ]
+        ):
+            raise ValueError(f"No button found with {text}")
+        else:
+            print(text)
+        return elems[0]
+
+    find_button("DOWNLOAD DATA").click()
+    find_button("METADATA (TSV)").click()
+    if (
+        file := Path.home() / "Downloads" / "nextstrain_monkeypox_hmpxv1_metadata.tsv"
+    ).exists():
+        return file
+    return None
+
+
+def upload(file: Path):
+    if file is None:
+        logging.error("Nextstrain file not downloaded")
+        return None
+    BUCKET = os.getenv("MONKEYPOX_BUCKET")
+    s3 = boto3.resource("s3")
+    today = datetime.today().date()
+    try:
+        s3.Object(BUCKET, f"{today}/nextstrain_monkeypox_hmpxv1_metadata.tsv").put(
+            Body=file.read_text()
+        )
+    except Exception as exc:
+        logging.exception("Failed to upload Nextstrain metadata")
+        raise
+
+
+if __name__ == "__main__":
+    upload(fetch_metadata(NEXTSTRAIN_MPXV))
