ab8d801f647a7f958337d2e827e6e38cbf042cdf
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2022-06-28 20:39:09 +0100

    figures(genomics): move correlation bottom-right, rename as r

--- src/figures/genomics.r
+++ src/figures/genomics.r
@@ -9,7 +9,8 @@ fig1 <- ggscatter(agg_df, x = "nextstrain_genome_count", y = "Gh_confirmed_cases
                   xlab = "Number of MPXV genomes", ylab = "Number of Confirmed Cases",
                   label = "Country", repel = TRUE,
                   font.label = "black") +
-  stat_cor(method = "pearson", label.x = 0, label.y = max(agg_df$Gh_confirmed_cases)+30) +
+  stat_cor(method = "pearson", label.y = 0, cor.coef.name = "r",
+           label.x = 0.6 * max(agg_df$nextstrain_genome_count)) +
   coord_cartesian(clip = 'off') +
   theme(text = element_text(colour = "black"))
 
